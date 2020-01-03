1b267b7295a084a9bb9d0959502186c01271f7ee
CollapseNav <jwpwdwd@163.com>
2020-01-03 22:57:25 +0800

    ......

--- Ng.Net.Application/UserApplication.cs
+++ Ng.Net.Application/UserApplication.cs
@@ -1,4 +1,12 @@
+/*
+ * @Author: CollapseNav
+ * @Date: 2019-12-30 20:02:06
+ * @LastEditors  : CollapseNav
+ * @LastEditTime : 2020-01-03 16:44:41
+ * @Description: 
+ */
 using System;
+using System.Linq;
 using Ng.Net.Application.Interface;
 using Ng.Net.Repository;
 using Ng.Net.Repository.Domain;
@@ -15,17 +23,30 @@ namespace Ng.Net.Application
             _rep = rep;
         }
 
-        public UserInfo GetUserInfobyUserAccount(string userAccount) => _rep.FindSingle(model => model.UserAccount == userAccount);
 
-        public UserInfo GetUserInfobyUserName(string userName) => _rep.FindSingle(model => model.UserName == userName);
+        public UserInfo GetUserInfo(string userid) => _rep.FindByID(userid);
+
+        /// <summary>
+        /// this func should not be here
+        /// but i do not want to write a new class or a new interface
+        /// :)
+        /// </summary>
+        /// <returns></returns>
+        public IQueryable<UserInfo> GetAllUser() => _rep.FindAll();
+
+        private UserInfo GetUserInfobyUserAccount(string userAccount) => _rep.FindSingle(model => model.UserAccount == userAccount);
+
+        private UserInfo GetUserInfobyUserName(string userName) => _rep.FindSingle(model => model.UserName == userName);
 
         public bool IsAccountExist(string userAccount) => _rep.IsExist(entity => entity.UserAccount == userAccount);
 
         public bool IsUserNameExist(string userName) => _rep.IsExist(entity => entity.UserName == userName);
 
-        public bool SignInbyAccount(UserInfo entity) => _rep.IsExist(model => model.UserAccount == entity.UserAccount);
+        public bool SignInbyAccount(UserInfo entity) => _rep.IsExist(model => model.UserAccount == entity.UserAccount && model.PassWord == entity.PassWord);
 
-        public bool SignInbyName(UserInfo entity) => _rep.IsExist(model => model.UserName == entity.UserName);
+        public bool SignInbyName(UserInfo entity) => _rep.IsExist(model => model.UserName == entity.UserName && model.PassWord == entity.PassWord);
+
+        public bool SignIn(UserInfo entity) => string.IsNullOrEmpty(entity.UserAccount) ? SignInbyName(entity) : SignInbyAccount(entity);
 
         public bool SignUp(UserInfo entity)
         {
